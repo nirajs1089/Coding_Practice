1220ab19dde8452d58592259c2ddca0664eb91ce
Niraj Shah <niraj.ns.shah@gmail.com>
2018-07-11 16:39:15 -0400

    easy question

---
+++ Last_Second-Last.py
@@ -0,0 +1,44 @@
+Given a string, create a new string made up of its last two letters, reversed and separated by a space. For example, the word is 'bat'. Return 't a'.
+
+ 
+
+Function Description 
+
+Complete the function lastLetters in the editor below. The function must return a string of two space-separated characters.
+
+ 
+
+lastLetters has the following parameter(s):
+
+    word:  a string
+
+ 
+
+Constraints
+
+1 ≤ length of word ≤ 100
+ 
+
+Input Format for Custom Testing
+Input from stdin will be processed as follows and passed to the function.
+
+ 
+
+The line contains a string word.
+
+ 
+
+Sample Case 0
+Sample Input 0
+
+APPLE
+ 
+
+Sample Output 0
+
+E L
+ 
+
+Explanation 0
+
+The last letter in APPLE is E and the second-last letter is L, so we return E L.
